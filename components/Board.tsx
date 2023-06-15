91bbd9e7bcbc4265212812e98354d78d4808cabd
김용민 <dydals3440@gmail.com>
2023-06-15 22:48:59 +0900

    feat: SetUp appwrite

---
+++ components/Board.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// react beautiful dnd
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+
+function Board() {
+  return (
+    <DragDropContext>
+      <Droppable
+        droppableId='board'
+        direction='horizontal'
+        type='column' {(provided) => (
+            <div>{/* rendering all the columns */}</div>
+        )}
+      ></Droppable>
+    </DragDropContext>
+  );
+}
+
+export default Board;
